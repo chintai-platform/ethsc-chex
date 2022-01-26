839af97349ede3c169d3dba7eb460fe2e22982e6
Phillip Hamnett <phillip.hamnett@chintai.io>
2022-01-26 16:51:58 +0100

    EX-979 Add ropsten configuration secrets and deploy script

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,10 @@
+const { endpoint, privateKey, etherscanAPIKey} = require('./.secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+
 module.exports = {
+  plugins: ['truffle-plugin-verify'],
+
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -6,20 +12,20 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-     development: {
+    development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
-     },
+    },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-    provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    network_id: 3,       // Ropsten's id
-    gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      provider: () => new HDWalletProvider(privateKey, endpoint),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     }
   },
 
@@ -34,4 +40,8 @@ module.exports = {
       version: "0.8.11"    // Fetch exact version from solc-bin (default: truffle's version)
     }
   },
+  api_keys: {
+    etherscan: etherscanAPIKey
+  }
+
 };
