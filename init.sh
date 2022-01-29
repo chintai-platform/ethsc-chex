35aa6872b2c90924dad7225c684d1303896e41af
Phillip Hamnett <phillip.hamnett@chintai.io>
2022-01-29 09:19:37 +0100

    EX-979 Saving progress for a test

--- init.sh
+++ init.sh
@@ -6,3 +6,6 @@ npm i @openzeppelin/contracts-upgradeable
 npm i --save-dev @openzeppelin/truffle-upgrades
 npm i -D truffle-plugin-verify
 npm i --save-dev chai
+npm i @openzeppelin/test-helpers
+npm i @openzeppelin/test-environment
+npm i ethers
