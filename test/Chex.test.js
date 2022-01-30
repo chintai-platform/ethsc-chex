0f77405444fa22a145c8b47b5e5a3842a641207f
Phillip Hamnett <phillip.hamnett@chintai.io>
2022-01-30 07:21:18 +0100

    EX-979 Add lots of tests

--- test/Chex.test.js
+++ test/Chex.test.js
@@ -45,6 +45,15 @@ contract('Chex', accounts => {
   const alice = accounts[1];
   const bob = accounts[2];
 
+  it('check initial values', async function () {
+    const contract = await chex.deployed();
+
+    expect((await contract.name()).toString()).to.equal('Chintai Exchange Token');
+    expect((await contract.symbol()).toString()).to.equal('CHEX');
+    expect((await contract.decimals()).toString()).to.equal('18');
+    expect((await contract.totalSupply()).toString()).to.equal('0');
+  });
+
   it('should mint tokens', async () => {
     const contract = await chex.deployed();
 
@@ -119,4 +128,46 @@ contract('Chex', accounts => {
     expect((await contract.balanceOf(alice)).toString()).to.equal('1000000000000000000');
     expect((await contract.balanceOf(bob)).toString()).to.equal('2500000000000000000');
   });
+
+  it('should burn tokens on request', async () => {
+    const contract = await chex.deployed();
+
+    await contract.burn('1000000000000000000', {from:alice});
+    expect((await contract.totalSupply()).toString()).to.equal('3000000000000000000');
+    expect((await contract.balanceOf(owner)).toString()).to.equal('500000000000000000');
+    expect((await contract.balanceOf(alice)).toString()).to.equal('0');
+    expect((await contract.balanceOf(bob)).toString()).to.equal('2500000000000000000');
+  });
+
+  it('should not burn tokens you do not have', async () => {
+    const contract = await chex.deployed();
+
+    await expectRevert(contract.burn('1000000000000000000', {from:alice}), 'ERC20: burn amount exceeds balance -- Reason given: ERC20: burn amount exceeds balance.');
+    expect((await contract.totalSupply()).toString()).to.equal('3000000000000000000');
+    expect((await contract.balanceOf(owner)).toString()).to.equal('500000000000000000');
+    expect((await contract.balanceOf(alice)).toString()).to.equal('0');
+    expect((await contract.balanceOf(bob)).toString()).to.equal('2500000000000000000');
+  });
+
+  it('should not burn tokens of someone else', async () => {
+    const contract = await chex.deployed();
+
+    await expectRevert(contract.burnFrom(owner, '10000000000000000', {from:bob}), 'ERC20: burn amount exceeds allowance -- Reason given: ERC20: burn amount exceeds allowance.');
+    expect((await contract.totalSupply()).toString()).to.equal('3000000000000000000');
+    expect((await contract.balanceOf(owner)).toString()).to.equal('500000000000000000');
+    expect((await contract.balanceOf(alice)).toString()).to.equal('0');
+    expect((await contract.balanceOf(bob)).toString()).to.equal('2500000000000000000');
+  });
+
+  it('should issue tokens with memo', async () => {
+    const contract = await chex.deployed();
+
+    await contract.issue(owner, '10000000000000000000', 'This is the memo', {from:owner});
+    expect((await contract.totalSupply()).toString()).to.equal('3000000000000000000');
+    expect((await contract.balanceOf(owner)).toString()).to.equal('500000000000000000');
+    expect((await contract.balanceOf(alice)).toString()).to.equal('0');
+    expect((await contract.balanceOf(bob)).toString()).to.equal('2500000000000000000');
+  });
+
+
 });
