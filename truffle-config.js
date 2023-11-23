65e5f89bf16574bd4b5b93bcea71ad490ba3c238
Davide Spataro <davide90.spataro@gmail.com>
2023-11-23 17:31:23 +0100

    work in progress

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
-const { endpointHTTPS, endpointWSS, privateKey, etherscanAPIKey} = require('./.secrets.json');
+//const { endpointHTTPS, endpointWSS, privateKey, etherscanAPIKey} = require('./.secrets.json');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
+require("dotenv").config();
+const { MNEMONIC, PROJECT_ID } = process.env;
 
 module.exports = {
   plugins: ['truffle-plugin-verify'],
@@ -17,21 +19,32 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-    live: {
-      provider: () => new HDWalletProvider(privateKey, endpointWSS),
-      network_id: 1,
-      gasPrice: 155000000000
-    },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    ropsten: {
-      provider: () => new HDWalletProvider(privateKey, endpointWSS),
-      network_id: 3,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    }
+    // live: {
+    //   provider: () => new HDWalletProvider(privateKey, endpointWSS),
+    //   network_id: 1,
+    //   gasPrice: 155000000000
+    // },
+    // // Useful for deploying to a public network.
+    // // NB: It's important to wrap the provider as a function.
+    // ropsten: {
+    //   provider: () => new HDWalletProvider(privateKey, endpointWSS),
+    //   network_id: 3,       // Ropsten's id
+    //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    //   confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // }
+    goerli: {
+      provider: () =>
+          new HDWalletProvider(
+              MNEMONIC,
+              `https://goerli.infura.io/v3/${PROJECT_ID}`,
+          ),
+      network_id: 5, // Goerli's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+  },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -45,8 +58,8 @@ module.exports = {
       version: "0.8.11"    // Fetch exact version from solc-bin (default: truffle's version)
     }
   },
-  api_keys: {
-    etherscan: etherscanAPIKey
-  }
+  // api_keys: {
+  //   etherscan: etherscanAPIKey
+  // }
 
 };
