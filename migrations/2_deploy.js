84cd25fb36ba8845d105036856679f020cd1ef10
Phillip Hamnett <phillip.hamnett@chintai.io>
2024-05-21 11:55:03 +0200

    Create upgrade migration file

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,12 +1,11 @@
 // migrations/NN_deploy_upgradeable_box.js
-const { deployProxy, upgradeProxy} = require('@openzeppelin/truffle-upgrades');
+const { forceImport, upgradeProxy} = require('@openzeppelin/truffle-upgrades');
 
 const Chex = artifacts.require('Chex');
 const ChexV2 = artifacts.require('ChexV2');
 
 module.exports = async function (deployer) {
-  const alreadyDeployed = await Chex.deployed();
-  console.log(alreadyDeployed);
-  const instance = await upgradeProxy(Chex.address, ChexV2, { deployer, initializer: 'initialize' });
+  const existingProxy = await forceImport("0xaED75A1c0c2a7d2bF0a0Ced267474AfeCe21897f", Chex, { deployer });
+  const instance = await upgradeProxy("0xaED75A1c0c2a7d2bF0a0Ced267474AfeCe21897f", ChexV2, { deployer, initializer: 'initialize' });
   console.log('Deployed', instance.address);
 };
