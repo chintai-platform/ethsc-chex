562da926b0664fdc9e6a9bb9018a38ccec0e54a2
Phillip Hamnett <phillip.hamnett@chintai.io>
2024-05-21 14:48:46 +0200

    Add initializer to migration script

--- migrations/3_upgrade_to_chexv2.js
+++ migrations/3_upgrade_to_chexv2.js
@@ -6,6 +6,6 @@ const ChexV2 = artifacts.require('ChexV2');
 
 module.exports = async function (deployer) {
   const deployedProxyAddress = process.env.DEPLOYED_PROXY_ADDRESS;
-  const upgraded = await upgradeProxy(deployedProxyAddress, ChexV2, { deployer });
+  const upgraded = await upgradeProxy(deployedProxyAddress, ChexV2, { deployer, initializer: 'initialize' });
   console.log('Upgraded', upgraded.address);
 }
